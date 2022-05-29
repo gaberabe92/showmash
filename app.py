72ed913cd3b72804ba7506a056a8371a5bc20dd2
gaberabe92 <gabriel.luechinger@stud.fhgr.ch>
2022-05-29 13:00:00 +0100

    Added routes

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask import (request,
+                   render_template,
+                   jsonify)
+
+from controllers import face_mash
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    """"Route to home page"""
+    shows, selected_players = face_mash.home_handler()
+    return render_template('index.html', 
+                            shows=shows, 
+                            selected_players=selected_players)
+
+@app.route("/ranking", methods=["GET"])
+def ranking():
+    """"Route to home page"""
+    shows, selected_players = face_mash.home_handler()
+    return render_template('randking.html', 
+                            shows=shows, 
+                            selected_players=selected_players)
+
+@app.route("/get_shows", methods=["POST"])
+def get_shows():
+    """Route to fetch all the shows"""
+    return jsonify(shows=face_mash.get_all_show())
+
+
+@app.route("/new_players", methods=["POST"])
+def new_players():
+    """"Route to return random two players"""
+    return jsonify(selected_players=face_mash.select_players())
+
+
+
+@app.route("/vote", methods=["POST"])
+def vote():
+    """Route to handle voting"""
+    data = request.get_json()
+    winner_id = data.get("winner_id")
+    looser_id = data.get("looser_id")
+    if not winner_id or not looser_id :
+        return jsonify(status=False,
+                       winner_elo=0,
+                       losser_elo=0,
+                       message="Please vote.")
+    win_elo, lose_elo = face_mash.vote_handler(winner_id, looser_id)
+    return jsonify(status=False,
+                   winner_elo=win_elo,
+                   losser_elo=lose_elo)
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", 
+            port=5000,debug=True)
