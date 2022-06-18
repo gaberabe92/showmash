1b43081934811e92051047d5a4451b956ddfff51
gaberabe92 <niklas@Niklas-MBP.fritz.box>
2022-06-18 15:08:37 +0200

    typos corrected

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import (request,
                    render_template,
                    jsonify)
 
-from controllers import face_mash
+from controllers import show_mash
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     """"Route to home page"""
-    shows, selected_players = face_mash.home_handler()
+    shows, selected_players = show_mash.home_handler()
     return render_template('index.html', 
                             shows=shows, 
                             selected_players=selected_players)
@@ -18,7 +18,7 @@ def home():
 @app.route("/ranking", methods=["GET"])
 def ranking():
     """"Route to home page"""
-    shows, selected_players = face_mash.home_handler()
+    shows, selected_players = show_mash.home_handler()
     return render_template('ranking.html',
                             shows=shows, 
                             selected_players=selected_players)
@@ -26,13 +26,13 @@ def ranking():
 @app.route("/get_shows", methods=["POST"])
 def get_shows():
     """Route to fetch all the shows"""
-    return jsonify(shows=face_mash.get_all_show())
+    return jsonify(shows=show_mash.get_all_show())
 
 
 @app.route("/new_players", methods=["POST"])
 def new_players():
     """"Route to return random two players"""
-    return jsonify(selected_players=face_mash.select_players())
+    return jsonify(selected_players=show_mash.select_players())
 
 
 
@@ -47,7 +47,7 @@ def vote():
                        winner_elo=0,
                        losser_elo=0,
                        message="Please vote.")
-    win_elo, lose_elo = face_mash.vote_handler(winner_id, looser_id)
+    win_elo, lose_elo = show_mash.vote_handler(winner_id, looser_id)
     return jsonify(status=False,
                    winner_elo=win_elo,
                    losser_elo=lose_elo)
